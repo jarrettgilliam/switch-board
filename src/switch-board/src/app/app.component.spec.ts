7d5ccb1ee1d6a96dc5c0824ae26fcdf569a4a709
Jarrett Gilliam <jarrettgilliam@gmail.com>
2020-04-14 20:31:55 -0500

    Rename switch-board-ui to switch-board

--- src/switch-board/src/app/app.component.spec.ts
+++ src/switch-board/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'switch-board-ui'`, () => {
+  it(`should have as title 'switch-board'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('switch-board-ui');
+    expect(app.title).toEqual('switch-board');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('switch-board-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('switch-board app is running!');
   });
 });
